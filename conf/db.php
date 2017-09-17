71f9741f0f757415d692a3a7a7849e6082a0c272
Sicaire Loïc <loic.sicaire@gmail.com>
2017-09-17 03:45:06 +0200

    Db local

--- conf/db.php
+++ conf/db.php
@@ -5,6 +5,10 @@ function connexionBdd(){
     $db = "db695868253";
     $user = "dbo695868253";
     $pass = "Matemaker33!";
+    $hote = "localhost";
+    $db = "mate_maker_api";
+    $user = "root";
+    $pass = "";
     try {
         $bdd = new PDO('mysql:host='.$hote.';dbname='.$db.';charset=utf8', $user, $pass);
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
